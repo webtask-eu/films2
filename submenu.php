1142d5494bfc5ae883b7beff58cc5595eea19eb3
webtask-eu <webtask.eu@gmail.com>
2023-06-12 16:29:57 +0300

    auto-commit

--- submenu.php
+++ submenu.php
@@ -1,12 +1,9 @@
-<nav class="submenu">
+
+<div class="submenu">
     <ul>
-        <?php if (is_logged_in()) { ?>
-            <li><a href="/collection_create.php"><?php echo translate('Add to Collection'); ?></a></li>
-            <li><a href="/profile.php"><?php echo translate('My Profile'); ?></a></li>
-            <li><a href="/logout.php"><?php echo translate('Logout'); ?></a></li>
-        <?php } else { ?>
-            <li><a href="/register.php"><?php echo translate('Register'); ?></a></li>
-            <li><a href="/login.php"><?php echo translate('Login'); ?></a></li>
-        <?php } ?>
+        <li><a href="/my_collections.php"><?php echo translate('My Collections'); ?></a></li>
+        <li><a href="/add_to_collection.php"><?php echo translate('Add to Collection'); ?></a></li>
+        <li class="active"><a href="/profile.php"><?php echo translate('My Profile'); ?></a></li>
+        <li><a href="/logout.php"><?php echo translate('Logout'); ?></a></li>
     </ul>
-</nav>
+</div>
