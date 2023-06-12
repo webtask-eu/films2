e6cecc82d8a0ebe760e118cc3f2de06b1217e346
webtask-eu <webtask.eu@gmail.com>
2023-06-12 12:33:46 +0300

    Revert "auto-commit"
    This reverts commit bc8422298ce6cff98c6fda83e162eb4291cd540d.

--- profile.php
+++ profile.php
@@ -1,66 +1,55 @@
-Извините за пропуск! Вот обновленный код `register.php`, включающий меню и учет путей в конфигурационном файле:
-
-```php
 <?php
-require_once __DIR__ . 'config.php';
+require_once __DIR__ . '/config.php';
 
-// Проверка, если пользователь уже авторизован, перенаправление на страницу профиля
-if (is_logged_in()) {
-    redirect('/profile.php');
+// Проверка авторизации пользователя
+if (!is_logged_in()) {
+    // Если пользователь не авторизован, перенаправляем на страницу входа
+    redirect('/login.php');
 }
 
-// Обработка формы регистрации
+// Получение информации о текущем пользователе
+$user_id = $_SESSION['user_id'];
+$user = get_user($user_id);
+
+// Обработка изменений данных пользователя
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Получение данных из формы
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Создание пользователя
-    $result = create_user($name, $email, $password);
-
-    if ($result) {
-        // Успешная регистрация, перенаправление на страницу профиля
-        redirect('/profile.php');
-    } else {
-        // Ошибка регистрации
-        $error_message = 'Registration failed. Please try again.';
-    }
+    // Обновление информации о пользователе
+    update_user($user_id, $name, $email, $password);
+    
+    // Перенаправление на страницу профиля
+    redirect('/profile.php');
 }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Registration</title>
+    <title>My Profile</title>
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <?php include ROOT_PATH . '/menu.php'; ?>
+    <?php include_once __DIR__ . '/includes/menu.php'; ?>
 
-    <div class="container">
-        <h1>Registration</h1>
-        <?php if (isset($error_message)) { ?>
-            <p class="error"><?php echo $error_message; ?></p>
-        <?php } ?>
-        <form method="POST" action="register.php">
-            <div>
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
-            </div>
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <div>
-                <button type="submit">Register</button>
-            </div>
+    <main>
+        <h1>My Profile</h1>
+        <form method="POST">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" value="<?php echo $user['name']; ?>" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" value="<?php echo $user['email']; ?>" required>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit">Save</button>
         </form>
-    </div>
+    </main>
 </body>
 </html>
