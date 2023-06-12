506745351d723f83724ccc45c5fa149da55d1273
webtask-eu <webtask.eu@gmail.com>
2023-06-12 12:04:08 +0300

    auto-commit

--- config.php
+++ config.php
@@ -3,6 +3,12 @@ define('PRIVATE_PATH', __DIR__ . '/private_html');
 define('INCLUDES_PATH', PRIVATE_PATH . '/includes');
 define('LOCALES_PATH', PRIVATE_PATH . '/../locales');
 
+// Подключение файла session_start.php
+require_once INCLUDES_PATH . '/session_start.php';
+
+// Подключение файла functions.php
+require_once INCLUDES_PATH . '/functions.php';
+
 // Включение отображения ошибок в PHP
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
