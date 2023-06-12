b9148db8c4afe0a347daa98974626865f3426842
webtask-eu <webtask.eu@gmail.com>
2023-06-12 11:42:10 +0300

    auto-commit

--- index.php
+++ index.php
@@ -41,16 +41,20 @@ $success_message = get_success_message();
 </head>
 <body>
 <header>
-  <nav>
-    <ul>
-      <li class="menu-items center"><a href="/register.php"><?php echo translate('Register'); ?></a></li>
-      <li class="menu-items center"><a href="/login.php"><?php echo translate('Login'); ?></a></li>
-      <li class="menu-items language"><a href="<?php echo update_query_param('lang', 'en'); ?>">EN</a></li>
-      <li class="menu-items language"><a href="<?php echo update_query_param('lang', 'lv'); ?>">LV</a></li>
-      <li class="menu-items language"><a href="<?php echo update_query_param('lang', 'ru'); ?>">RU</a></li>
-    </ul>
-  </nav>
+    <nav>
+        <ul>
+            <li class="site-title"><a href="/index.php"><?php echo translate('Film Collection'); ?></a></li>
+            <li class="right"><a href="/register.php"><?php echo translate('Register'); ?></a></li>
+            <li class="right"><a href="/login.php"><?php echo translate('Login'); ?></a></li>
+            <li class="center"><a href="/collection_create.php"><?php echo translate('Create Collection'); ?></a></li>
+            <li class="center"><a href="/collection_view.php"><?php echo translate('My Collections'); ?></a></li>
+            <li class="language"><a href="<?php echo update_query_param('lang', 'en'); ?>">EN</a></li>
+            <li class="language"><a href="<?php echo update_query_param('lang', 'lv'); ?>">LV</a></li>
+            <li class="language"><a href="<?php echo update_query_param('lang', 'ru'); ?>">RU</a></li>
+        </ul>
+    </nav>
 </header>
+
 
 
     <main>
