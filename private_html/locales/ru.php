127a91b9ae61a30f6e202ee7fc748222309d3f68
webtask-eu <webtask.eu@gmail.com>
2023-06-12 13:14:14 +0300

    auto-commit

--- private_html/locales/ru.php
+++ private_html/locales/ru.php
@@ -1,18 +1,20 @@
 <?php
+
 return [
     'Home' => 'Главная',
     'Register' => 'Регистрация',
     'Login' => 'Вход',
+    'Create Collection' => 'Создать коллекцию',
     'EN' => 'EN',
     'LV' => 'LV',
     'RU' => 'RU',
-    'Film Collection' => 'Коллекция фильмов',
     'Latest Movies' => 'Последние фильмы',
     'No movies found.' => 'Фильмы не найдены.',
+    'My Collections' => 'Мои коллекции',
+    'Film Collection' => 'Коллекция фильмов',
     'Name' => 'Имя',
     'Email' => 'Email',
     'Password' => 'Пароль',
-    'Save' => 'Сохранить',
-    'My Profile' => 'Мой профиль',
 ];
+
 ?>
