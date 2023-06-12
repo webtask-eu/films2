f743f487116616fd7a0b09b42cff745ae3bf6818
webtask-eu <webtask.eu@gmail.com>
2023-06-12 12:06:22 +0300

    auto-commit

--- menu.php
+++ menu.php
@@ -1,15 +1,12 @@
 <nav>
-    <ul>
-        <li><a href="/index.php"><?php echo translate('Home'); ?></a></li>
-        <?php if (is_logged_in()) { ?>
-            <li><a href="/collection_create.php"><?php echo translate('Create Collection'); ?></a></li>
-            <li><a href="/my_collections.php"><?php echo translate('My Collections'); ?></a></li>
-        <?php } else { ?>
-            <li><a href="/register.php"><?php echo translate('Register'); ?></a></li>
-            <li><a href="/login.php"><?php echo translate('Login'); ?></a></li>
-        <?php } ?>
-        <li><a href="<?php echo update_query_param('lang', 'en'); ?>">EN</a></li>
-        <li><a href="<?php echo update_query_param('lang', 'lv'); ?>">LV</a></li>
-        <li><a href="<?php echo update_query_param('lang', 'ru'); ?>">RU</a></li>
-    </ul>
-</nav>
+        <ul>
+            <li class="site-title"><a href="/index.php"><?php echo translate('Film Collection'); ?></a></li>
+            <li class="right"><a href="/register.php"><?php echo translate('Register'); ?></a></li>
+            <li class="right"><a href="/login.php"><?php echo translate('Login'); ?></a></li>
+            <li class="center"><a href="/collection_create.php"><?php echo translate('Create Collection'); ?></a></li>
+            <li class="center"><a href="/collection_view.php"><?php echo translate('My Collections'); ?></a></li>
+            <li class="language"><a href="<?php echo update_query_param('lang', 'en'); ?>">EN</a></li>
+            <li class="language"><a href="<?php echo update_query_param('lang', 'lv'); ?>">LV</a></li>
+            <li class="language"><a href="<?php echo update_query_param('lang', 'ru'); ?>">RU</a></li>
+        </ul>
+    </nav>
