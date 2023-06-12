3b5ef802332d4c00df4f16c380d517f06ce98246
webtask-eu <webtask.eu@gmail.com>
2023-06-12 10:25:44 +0300

    g

--- private_html/includes/config.php
+++ private_html/includes/config.php
@@ -2,3 +2,8 @@
 define('PRIVATE_PATH', __DIR__);
 define('INCLUDES_PATH', PRIVATE_PATH . '/includes');
 define('LOCALES_PATH', PRIVATE_PATH . '/../locales');
+
+// Включение отображения ошибок в PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
