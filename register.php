554d79aad7436b502b5cf0e75c4272446818cf98
webtask-eu <webtask.eu@gmail.com>
2023-06-12 12:11:46 +0300

    auto-commit

--- register.php
+++ register.php
@@ -1,6 +1,23 @@
 <?php
 require_once __DIR__ . '/config.php';
 
+// Загрузка переводов
+$translations = load_translations();
+
+// Получение языка из параметра GET
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+
+    // Установка языка
+    set_language($lang);
+
+    // Перенаправление на главную страницу
+    redirect('/index.php');
+}
+
+// Получение текущего языка
+$currentLanguage = get_language();
+
 // Проверка, если пользователь уже авторизован, перенаправляем на главную страницу
 if (is_logged_in()) {
     redirect('/index.php');
