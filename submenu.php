e5e40b8db44c97c3a811fa4870e19f6cd1f46171
webtask-eu <webtask.eu@gmail.com>
2023-06-12 16:36:16 +0300

    auto-commit

--- submenu.php
+++ submenu.php
@@ -1,7 +1,6 @@
-
 <ul class="submenu">
-    <li><a href="/profile.php"><?php echo translate('My Profile'); ?></a></li>
-    <li><a href="/my_collections.php"><?php echo translate('My Collections'); ?></a></li>
-    <li><a href="/add_movie.php"><?php echo translate('Add Movie'); ?></a></li>
-    <li><a href="/logout.php"><?php echo translate('Logout'); ?></a></li>
+  <li><a href="/profile.php"><?php echo translate('My Profile'); ?></a></li>
+  <li><a href="/collections.php"><?php echo translate('My Collections'); ?></a></li>
+  <li class="center-items"><a href="/add_movie.php"><?php echo translate('Add Movie'); ?></a></li>
+  <li><a href="/logout.php"><?php echo translate('Logout'); ?></a></li>
 </ul>
