4871bfa12eb7efc7cb2637697d32babbb0788185
webtask-eu <webtask.eu@gmail.com>
2023-06-16 23:52:29 +0300

    auto-commit

--- collections.php
+++ collections.php
@@ -1,20 +1,14 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-// Получение ID коллекции из параметров запроса
-$collection_id = $_GET['id'];
-
-// Получение информации о коллекции
-$collection = get_collection($collection_id);
-
-// Проверка, если коллекция не найдена, выводим ошибку
-if (!$collection) {
-    echo 'Collection not found.';
-    exit;
+// Проверка авторизации пользователя
+if (!is_logged_in()) {
+    redirect('/login.php');
 }
 
-// Debug Info
-// var_dump($collection);
+// Получение списка коллекций пользователя
+$user_id = $_SESSION['user_id'];
+$collections = get_user_collections($user_id);
 
 ?>
 
@@ -22,21 +16,28 @@ if (!$collection) {
 <html>
 <head>
     <meta charset="UTF-8">
-    <title><?php echo translate('Collection'); ?></title>
+    <title><?php echo translate('My Collections'); ?></title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/collections.css">
 </head>
 <body>
     <header>
-        <?php include_once __DIR__ . '/menu.php'; ?>
+        <?php include_once __DIR__ . '/private_html/includes/menu.php'; ?>
     </header>
     <main>
-        <div class="collection">
-            <?php if (isset($collection['name'])) { ?>
-                <h2><?php echo $collection['name']; ?></h2>
+        <h1><?php echo translate('My Collections'); ?></h1>
+        <div class="collection-list">
+            <?php if (empty($collections)) { ?>
+                <p><?php echo translate('No collections found.'); ?></p>
+            <?php } else { ?>
+                <?php foreach ($collections as $collection) { ?>
+                    <div class="collection-item">
+                        <h2><?php echo htmlspecialchars($collection['name']); ?></h2>
+                        <p><?php echo translate('Movie Count'); ?>: <?php echo $collection['movie_count']; ?></p>
+                        <a href="/collection.php?id=<?php echo $collection['id']; ?>"><?php echo translate('View Collection'); ?></a>
+                    </div>
+                <?php } ?>
             <?php } ?>
-            <p><?php echo $collection['description']; ?></p>
-            <a href="/collection.php?id=<?php echo $collection['id']; ?>"><?php echo translate('View Collection'); ?></a>
         </div>
     </main>
 </body>
