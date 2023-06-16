8d1e9fb34843dc33b950ee6b59e7a6f202abb094
webtask-eu <webtask.eu@gmail.com>
2023-06-17 00:33:22 +0300

    auto-commit

--- add_movie.php
+++ add_movie.php
@@ -7,36 +7,50 @@ if (!is_logged_in()) {
     redirect('/login.php');
 }
 
-// Проверка, что передан параметр collection_id в URL
-if (isset($_GET['collection_id'])) {
-    $collection_id = $_GET['collection_id'];
+// Получение ID коллекции из параметров запроса
+$collection_id = $_GET['collection_id'];
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Получение данных из формы
-        $movie_id = $_POST['movie_id'];
+// Проверка наличия коллекции в базе данных
+$collection = get_collection($collection_id);
 
-        if (!empty($movie_id)) {
-            // Вызов функции add_movie_to_collection()
-            try {
-                add_movie_to_collection($collection_id, $movie_id);
-                echo "Movie successfully added to collection.";
-            } catch (Exception $e) {
-                echo "Failed to add movie to collection: " . $e->getMessage();
-            }
-        } else {
-            echo "Error: Invalid movie ID.";
+if (!$collection) {
+    // Коллекция не найдена, перенаправляем на страницу ошибки
+    $error_message = 'Collection not found.';
+}
+
+// Переменные для хранения данных формы
+$movie_id = '';
+$error_message = '';
+$success_message = '';
+
+// Обработка отправки формы
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Получение данных из формы
+    $movie_id = $_POST['movie_id'];
+
+    // Проверка наличия фильма в базе данных
+    $movie = get_movie($movie_id);
+
+    if (!$movie) {
+        $error_message = 'Movie not found.';
+    } else {
+        // Добавление фильма в коллекцию
+        try {
+            add_movie_to_collection($collection_id, $movie_id, $movie['language']);
+            $success_message = 'Movie successfully added to collection.';
+        } catch (Exception $e) {
+            $error_message = 'Failed to add movie to collection: ' . $e->getMessage();
         }
     }
-} else {
-    echo "Error: Invalid collection ID.";
 }
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Add Movie to Collection</title>
+    <title><?php echo translate('Add Movie'); ?></title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/add_movie.css">
 </head>
@@ -45,13 +59,24 @@ if (isset($_GET['collection_id'])) {
         <?php include_once __DIR__ . '/menu.php'; ?>
     </header>
     <main>
-        <h1>Add Movie to Collection</h1>
-        <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . '?collection_id=' . $collection_id); ?>">
+        <h1><?php echo translate('Add Movie'); ?></h1>
+        <?php if ($error_message) { ?>
+            <p class="error"><?php echo $error_message; ?></p>
+        <?php } ?>
+        <?php if ($success_message) { ?>
+            <p class="success"><?php echo $success_message; ?></p>
+        <?php } ?>
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) . '?collection_id=' . $collection_id; ?>">
             <div class="form-group">
-                <label for="movie_id">Movie ID:</label>
-                <input type="text" id="movie_id" name="movie_id" required>
+                <label for="movie_id"><?php echo translate('Movie'); ?>:</label>
+                <select id="movie_id" name="movie_id">
+                    <option value="">-- <?php echo translate('Select Movie'); ?> --</option>
+                    <?php foreach (get_movies() as $movie) { ?>
+                        <option value="<?php echo $movie['id']; ?>"><?php echo $movie['title']; ?></option>
+                    <?php } ?>
+                </select>
             </div>
-            <button type="submit">Add Movie</button>
+            <button type="submit"><?php echo translate('Add'); ?></button>
         </form>
     </main>
 </body>
