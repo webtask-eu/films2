4234eb5de6dcebcea5ad49cb786919298ac5ce31
webtask-eu <webtask.eu@gmail.com>
2023-06-16 23:36:38 +0300

    auto-commit

--- collection_create.php
+++ collection_create.php
@@ -1,6 +1,14 @@
 <?php
 require_once __DIR__ . '/config.php';
 
+// Проверка авторизации пользователя
+if (!is_logged_in()) {
+    redirect('/login.php');
+}
+
+// Получение ID текущего пользователя
+$user_id = $_SESSION['user_id'];
+
 // Переменные для хранения данных формы
 $name = '';
 $description = '';
@@ -13,55 +21,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $description = $_POST['description'];
 
     // Валидация данных
-    if (empty($name)) {
-        $error_message = 'Please enter a name for the collection.';
+    if (empty($name) || empty($description)) {
+        $error_message = 'Please fill in all fields.';
     } else {
         // Создание коллекции
-        $result = create_collection($name, $description);
+        $result = create_collection($user_id, $name, $description);
 
-        // Проверка результата создания коллекции
         if ($result['success']) {
-            // Коллекция успешно создана, перенаправление на страницу с коллекцией
-            redirect('/collections.php');
+            // Перенаправление на страницу коллекции
+            header("Location: /collections.php?collection_id=" . $result['collection_id']);
+            exit();
         } else {
-            // Произошла ошибка при создании коллекции
             $error_message = $result['message'];
         }
     }
 }
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title><?php echo translate('Create Collection'); ?></title>
+    <title>Create Collection</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/collection_create.css">
 </head>
 <body>
-    <header>
-        <?php include_once __DIR__ . '/menu.php'; ?>
-    </header>
-    <main>
-        <div class="container">
-            <h1><?php echo translate('Create Collection'); ?></h1>
-            <?php if ($error_message) { ?>
-                <p class="error"><?php echo $error_message; ?></p>
-            <?php } ?>
-            <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-                <div class="form-group">
-                    <label for="name"><?php echo translate('Name'); ?>:</label>
-                    <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($name); ?>">
-                </div>
-                <div class="form-group">
-                    <label for="description"><?php echo translate('Description'); ?>:</label>
-                    <textarea id="description" name="description"><?php echo htmlspecialchars($description); ?></textarea>
-                </div>
-                <button type="submit"><?php echo translate('Create'); ?></button>
-            </form>
+<header>
+    <?php include_once __DIR__ . '/private_html/includes/menu.php'; ?>
+</header>
+<main>
+    <h1>Create Collection</h1>
+    <?php if ($error_message) { ?>
+        <p class="error"><?php echo $error_message; ?></p>
+    <?php } ?>
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($name); ?>">
         </div>
-    </main>
+        <div class="form-group">
+            <label for="description">Description:</label>
+            <textarea id="description" name="description"><?php echo htmlspecialchars($description); ?></textarea>
+        </div>
+        <button type="submit">Create</button>
+    </form>
+</main>
 </body>
 </html>
