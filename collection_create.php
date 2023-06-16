8a9704ec604ebe0694c7394afaf3c37fc603f97c
webtask-eu <webtask.eu@gmail.com>
2023-06-16 23:05:26 +0300

    auto-commit

--- collection_create.php
+++ collection_create.php
@@ -1,4 +1,61 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-</html>if (!is_logged_in()) {
+// Проверка авторизации пользователя
+if (!is_logged_in()) {
+    redirect('/login.php');
+}
+
+// Обработка отправки формы
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Получение данных из формы
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+
+    try {
+        // Автоматический перевод названия коллекции на другие языки
+        $translatedName = translate_text($name, $_SESSION['language']);
+
+        // Создание коллекции
+        $collectionId = create_collection($name, $description, $translatedName);
+
+        // Перенаправление на страницу с информацией о коллекции
+        redirect('/collection.php?id=' . $collectionId);
+    } catch (Exception $e) {
+        // Обработка ошибки 500
+        // Вывод сообщения об ошибке или другие действия
+
+        // Пример вывода сообщения об ошибке
+        echo 'Ошибка: ' . $e->getMessage();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo translate('Create Collection'); ?></title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/collection_create.css">
+</head>
+<body>
+    <header>
+        <?php include_once __DIR__ . '/menu.php'; ?>
+    </header>
+    <main>
+        <h1><?php echo translate('Create Collection'); ?></h1>
+        <form method="POST">
+            <div class="form-group">
+                <label for="name"><?php echo translate('Name'); ?>:</label>
+                <input type="text" id="name" name="name" value="<?php echo isset($_POST['name']) ? htmlspecialchars($_POST['name']) : ''; ?>">
+            </div>
+            <div class="form-group">
+                <label for="description"><?php echo translate('Description'); ?>:</label>
+                <textarea id="description" name="description"><?php echo isset($_POST['description']) ? htmlspecialchars($_POST['description']) : ''; ?></textarea>
+            </div>
+            <button type="submit"><?php echo translate('Create'); ?></button>
+        </form>
+    </main>
+</body>
+</html>
