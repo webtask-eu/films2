b5a798d4cb12faf66e5f1bbae0735cb73bfd309c
webtask-eu <webtask.eu@gmail.com>
2023-06-19 12:38:52 +0300

    auto-commit

--- submenu.php
+++ submenu.php
@@ -1,7 +1,17 @@
 <ul class="submenu">
-  <li class="left-items"><a href="/collection_create.php"><?php echo translate('Create Collection'); ?></a></li>   
-  <li class="left-items"><a href="/add_movie.php"><?php echo translate('Add Movie'); ?></a></li>
-  <li class="left-items"><a href="/collections.php"><?php echo translate('My Collections'); ?></a></li>
-  <li class="right-items"><a href="/profile.php"><?php echo translate('My Profile'); ?></a></li>
-  <li class="right-items"><a href="/logout.php"><?php echo translate('Logout'); ?></a></li>
+  <li class="left-items<?php echo ($active_page === 'collection_create.php') ? ' active' : ''; ?>">
+    <a href="/collection_create.php"><?php echo translate('Create Collection'); ?></a>
+  </li>   
+  <li class="left-items<?php echo ($active_page === 'add_movie.php') ? ' active' : ''; ?>">
+    <a href="/add_movie.php"><?php echo translate('Add Movie'); ?></a>
+  </li>
+  <li class="left-items<?php echo ($active_page === 'collections.php') ? ' active' : ''; ?>">
+    <a href="/collections.php"><?php echo translate('My Collections'); ?></a>
+  </li>
+  <li class="right-items<?php echo ($active_page === 'profile.php') ? ' active' : ''; ?>">
+    <a href="/profile.php"><?php echo translate('My Profile'); ?></a>
+  </li>
+  <li class="right-items<?php echo ($active_page === 'logout.php') ? ' active' : ''; ?>">
+    <a href="/logout.php"><?php echo translate('Logout'); ?></a>
+  </li>
 </ul>
