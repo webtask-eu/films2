662531c45a2ff847748890e90ba846469c78e9ef
webtask-eu <webtask.eu@gmail.com>
2023-06-19 12:52:36 +0300

    auto-commit

--- collections.php
+++ collections.php
@@ -3,13 +3,15 @@ require_once __DIR__ . '/config.php';
 
 // Проверка авторизации пользователя
 if (!is_logged_in()) {
-    // Если пользователь не авторизован, перенаправляем на страницу входа
     redirect('/login.php');
 }
 
-// Получение коллекций пользователя
-$user_id = $_SESSION['user_id'];
-$collections = get_user_collections($user_id);
+// Получение списка коллекций пользователя
+try {
+    $collections = get_user_collections();
+} catch (Exception $e) {
+    $error_message = 'Failed to get user collections: ' . $e->getMessage();
+}
 
 ?>
 
@@ -17,7 +19,7 @@ $collections = get_user_collections($user_id);
 <html>
 <head>
     <meta charset="UTF-8">
-    <title><?php echo translate('My Collections'); ?></title>
+    <title>My Collections</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/collections.css">
 </head>
@@ -26,22 +28,33 @@ $collections = get_user_collections($user_id);
         <?php include_once __DIR__ . '/menu.php'; ?>
     </header>
     <main>
-        <h1><?php echo translate('My Collections'); ?></h1>
-        <?php if (count($collections) > 0) { ?>
-            <ul class="collection-list">
-                <?php foreach ($collections as $collection) { ?>
-                    <li>
-                        <a href="/collection.php?id=<?php echo $collection['id']; ?>">
-                            <div class="collection-details">
-                                <h2><?php echo $collection['name']; ?></h2>
-                                <p><?php echo translate('Movie Count'); ?>: <?php echo isset($collection['movie_count']) ? $collection['movie_count'] : 0; ?></p>
-                            </div>
-                        </a>
-                    </li>
-                <?php } ?>
-            </ul>
+        <h1>My Collections</h1>
+        <?php if (!empty($error_message)) { ?>
+            <p class="error"><?php echo $error_message; ?></p>
         <?php } else { ?>
-            <p><?php echo translate('No collections found.'); ?></p>
+            <?php if (empty($collections)) { ?>
+                <p>You don't have any collections. <a href="/collection_create.php">Create a collection</a></p>
+            <?php } else { ?>
+                <?php foreach ($collections as $collection) { ?>
+                    <h2><?php echo $collection['name']; ?></h2>
+                    <?php
+                    try {
+                        $movies = get_collection_movies($collection['id']);
+                    } catch (Exception $e) {
+                        $error_message = 'Failed to get collection movies: ' . $e->getMessage();
+                    }
+                    ?>
+                    <?php if (!empty($movies)) { ?>
+                        <ul>
+                            <?php foreach ($movies as $movie) { ?>
+                                <li><?php echo $movie['title']; ?></li>
+                            <?php } ?>
+                        </ul>
+                    <?php } else { ?>
+                        <p>No movies in this collection.</p>
+                    <?php } ?>
+                <?php } ?>
+            <?php } ?>
         <?php } ?>
     </main>
 </body>
