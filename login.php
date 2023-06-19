813f9f345ab5af5291c2395d79f459c2d9b41237
webtask-eu <webtask.eu@gmail.com>
2023-06-19 13:05:47 +0300

    auto-commit

--- login.php
+++ login.php
@@ -62,8 +62,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <button type="submit"><?php echo translate('Login'); ?></button>
         </form>
         <div class="links">
-            <a href="/registration.php"><?php echo translate('Register'); ?></a> |
-            <a href="/password_recovery.php"><?php echo translate('Forgot password'); ?></a>
+            <a class="register-link" href="/registration.php"><?php echo translate('Register'); ?></a>
+            <span class="separator">|</span>
+            <a class="forgot-password-link" href="/password_recovery.php"><?php echo translate('Forgot password'); ?></a>
         </div>
     </div>
 </body>
