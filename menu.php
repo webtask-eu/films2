a4fe82f8e7081576318f1d7bdcebdcae49971845
webtask-eu <webtask.eu@gmail.com>
2023-06-19 13:14:32 +0300

    auto-commit

--- menu.php
+++ menu.php
@@ -1,8 +1,6 @@
 <nav>
         <ul>
             <li class="site-title"><a href="/index.php"><?php echo translate('Film Collection'); ?></a></li>
-            <li class="right"><a href="/register.php"><?php echo translate('Register'); ?></a></li>
-            <li class="right"><a href="/login.php"><?php echo translate('Login'); ?></a></li>
             <li class="center"><a href="/collection_create.php"><?php echo translate('Create Collection'); ?></a></li>
             <li class="center"><a href="/collections.php"><?php echo translate('My Collections'); ?></a></li>
             <li class="language"><a href="<?php echo update_query_param('lang', 'en'); ?>">EN</a></li>
