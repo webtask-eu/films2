ca065016d29906290b080b41dc993a9778e9a145
webtask-eu <webtask.eu@gmail.com>
2023-06-19 20:13:16 +0300

    auto-commit

--- private_html/locales/ru.php
+++ private_html/locales/ru.php
@@ -25,6 +25,11 @@ return [
     'View Collection' => 'Просмотр коллекции',
     'Register' => 'Регистрация',
     'Forgot Password?' => 'Забыли пароль?',
+    'You don\'t have any collections. <a href="/collection_create.php">Create a collection</a>' => 'У вас нет ни одной коллекции. <a href="/collection_create.php">Создать коллекцию</a>',
+    'Please enter a title for the movie.' => 'Пожалуйста, введите название фильма.',
+    'Please select a collection.' => 'Пожалуйста, выберите коллекцию.',
+    'Failed to add movie to collection: ' => 'Не удалось добавить фильм в коллекцию: ',
+    'Add Movie' => 'Добавить фильм',
 
 ];
 
