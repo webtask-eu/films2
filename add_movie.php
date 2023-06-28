7677f18c4bfdeaa94a0064a108586a116f6b3720
webtask-eu <webtask.eu@gmail.com>
2023-06-28 16:27:44 +0300

    auto-commit

--- add_movie.php
+++ add_movie.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/config.php';
 
+$currentLanguage = get_current_language();
+
 // Проверка авторизации пользователя
 if (!is_logged_in()) {
     redirect('/login.php');
