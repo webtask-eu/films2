e383190e4a32dbf880a4e60d9b936314e7491c61
webtask-eu <webtask.eu@gmail.com>
2023-06-28 14:24:31 +0300

    auto-commit

--- add_movie.php
+++ add_movie.php
@@ -143,42 +143,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     </main>
     <script>
-        var movieSuggestions = []; // Инициализация переменной movieSuggestions
+    var movieSuggestions = [];
 
-        function getMovieSuggestions(query) {
-            const apiKey = '<?php echo TMDB_API_KEY; ?>';
-            const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+    function getMovieSuggestions(query) {
+        const apiKey = '<?php echo $tmdbApiKey; ?>';
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
 
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    const suggestionsContainer = document.getElementById('suggestions');
-                    suggestionsContainer.innerHTML = '';
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                movieSuggestions = data.results; // Сохранение результатов поиска в переменной movieSuggestions
 
-                    data.results.forEach(movie => {
-                        const li = document.createElement('li');
-                        li.textContent = movie.title;
-                        suggestionsContainer.appendChild(li);
-                    });
-                })
-                .catch(error => {
-                    console.error('Failed to fetch movie suggestions:', error);
+                const suggestionsContainer = document.getElementById('suggestions');
+                suggestionsContainer.innerHTML = '';
+
+                movieSuggestions.forEach(movie => {
+                    const li = document.createElement('li');
+                    li.textContent = movie.title;
+                    suggestionsContainer.appendChild(li);
                 });
-        }
+            })
+            .catch(error => {
+                console.error('Failed to fetch movie suggestions:', error);
+            });
+    }
 
-        function selectMovie(event) {
-            const selectedTitle = event.target.textContent;
-            const selectedMovie = movieSuggestions.find(movie => movie.title === selectedTitle);
+    function selectMovie(event) {
+        const selectedTitle = event.target.textContent;
+        const selectedMovie = movieSuggestions.find(movie => movie.title === selectedTitle);
 
-            if (selectedMovie) {
-                document.getElementById('title').value = selectedMovie.title;
-                document.getElementById('poster').value = `https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`;
-                document.getElementById('poster-preview').src = `https://image.tmdb.org/t/p/w200${selectedMovie.poster_path}`;
-                document.getElementById('poster-preview').style.display = 'block';
-                document.getElementById('description').value = selectedMovie.overview;
-            }
+        if (selectedMovie) {
+            document.getElementById('title').value = selectedMovie.title;
+            document.getElementById('poster').value = `https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`;
+            document.getElementById('poster-preview').src = `https://image.tmdb.org/t/p/w200${selectedMovie.poster_path}`;
+            document.getElementById('poster-preview').style.display = 'block';
+            document.getElementById('description').value = selectedMovie.overview;
         }
-    </script>
+    }
+</script>
+
 </body>
 </html>
 
