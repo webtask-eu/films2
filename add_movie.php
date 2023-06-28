8d009dc33d2ffa7941fd08b7d3d71d9a20fa879d
webtask-eu <webtask.eu@gmail.com>
2023-06-28 14:02:37 +0300

    auto-commit

--- add_movie.php
+++ add_movie.php
@@ -84,7 +84,7 @@
     </main>
     <script>
         function getMovieSuggestions(query) {
-            const apiKey = 'YOUR_TMDB_API_KEY';
+            const apiKey = 'fca80a35e9a4bccbf9a300c8e938e3e0';
             const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
 
             fetch(url)
